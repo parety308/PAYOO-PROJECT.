ee30f2a79ce3ca5ebb4f98595c5bf636d84af4da
parvez <parvezyesrat17032024@gmail.com>
2025-08-29 01:37:15 +0600

    added full code

--- payooProjectPart_1_Full.js
+++ payooProjectPart_1_Full.js
@@ -1,58 +1,47 @@
-
-//Add Money Section
-document.getElementById('addMoneyBtn').addEventListener('click', function (event) {
-    event.preventDefault();
-
-    //anlam
-    let money = document.getElementById('totalMoney');
-    const addMoney = document.getElementById('addMoney');
-
-    const accountNumber = document.getElementById('accountNumber').value;
-    let pinNumber = document.getElementById('pinNumber');
+function getInt(money) {
+    return parseInt(document.getElementById(money).value);
+}
+function validity(ac, p) {
+    const accountNumber = document.getElementById(ac).value;
+    let pinNumber = document.getElementById(p);
     pinNumber = parseInt(pinNumber.value);
-    if (accountNumber.length != 10) {
+    if (accountNumber.length !== 10) {
         alert('Invalid Account Number');
         return;
     }
-
-    if (pinNumber != 2205) {
+    if (pinNumber !== 2205) {
         alert('Invalid Pin');
+        return;
     }
+}
+function addIntT(moneyT, money) {
+    let addMoney = document.getElementById(moneyT);
+    const availableMoney = parseInt(addMoney.innerText) + money;
+    addMoney.innerText = availableMoney;
+}
 
-    //convert
-    const addedMoney = parseInt(addMoney.value);
-    let totalM = parseInt(money.innerText);
-    totalM += addedMoney;
-    money.innerText = totalM;
-
+function subIntT(moneyT, money) {
+    let subMoney = document.getElementById(moneyT);
+    const availableMoney = parseInt(subMoney.innerText) - money;
+    subMoney.innerText = availableMoney;
+}
+//Add Money Section
+document.getElementById('addMoneyBtn').addEventListener('click', function (event) {
+    event.preventDefault();
+    const addMoney = getInt('addMoney')
+    //validity
+    validity('accountNumber', 'pinNumber');
+    addIntT('totalMoney', addMoney);
 });
 
 //Cashout Section
 document.getElementById('cashOutBtn').addEventListener('click', function (event) {
     event.preventDefault();
+    const outMoney = getInt('cashoutMoney')
 
-    //anlam
-    let money = document.getElementById('totalMoney');
-    const cashout = document.getElementById('cashoutMoney');
-
-    const agentNumber = document.getElementById('agentNumber').value;
-    let pinNumber = document.getElementById('pinNumber');
-    pinNumber = parseInt(pinNumber.value);
-    if (agentNumber.length != 10) {
-        alert('Invalid Agent Number');
-        return;
-    }
-
-    if (pinNumber != 2205) {
-        alert('Invalid Pin');
-    }
-
-    //convert
-    const cashoutMoney = parseInt(cashout.value);
-    let totalM = parseInt(money.innerText);
-    totalM -= cashoutMoney;
-    money.innerText = totalM;
-
+    //validity
+    validity('agentNumber', 'pinNumber');
+    subIntT('totalMoney', outMoney);
 });
 
 //Tansfer Money
@@ -60,26 +49,10 @@ document.getElementById('transferBtn').addEventListener('click', function (event
     event.preventDefault();
 
     //anlam
-    let money = document.getElementById('totalMoney');
-    const transfer = document.getElementById('transferMoney');
+    const transferMoney = getInt('transferMoney');
 
-    const accountNumber = document.getElementById('accountNumber').value;
-    let pinNumber = document.getElementById('pinNumber');
-    pinNumber = parseInt(pinNumber.value);
-    if (accountNumber.length != 10) {
-        alert('Invalid Agent Number');
-        return;
-    }
-
-    if (pinNumber != 2205) {
-        alert('Invalid Pin');
-    }
-
-    //convert
-    const transferMoney = parseInt(transfer.value);
-    let totalM = parseInt(money.innerText);
-    totalM -= transferMoney;
-    money.innerText = totalM;
+    subIntT('totalMoney', transferMoney);
+    validity('accountNumber', 'pinNumber');
 
 });
 
@@ -88,18 +61,13 @@ document.getElementById('bonusBtn').addEventListener('click', function (event) {
     event.preventDefault();
 
     //anlam
-    let money = document.getElementById('totalMoney');
-    const cupon = parseInt(document.getElementById('cupon').value);
-    if (cupon != 2205) {
+
+    const Cupon = parseInt(document.getElementById('cupon').value);
+    if (Cupon != 2205) {
         alert('Invalid Cupon');
         return;
     }
-
-    //convert
-    let totalM = parseInt(money.innerText);
-    totalM += 1000;
-    money.innerText = totalM;
-
+    addIntT('cupon', 1000);
 });
 
 //Pay Bill
@@ -107,26 +75,10 @@ document.getElementById('payBillBtn').addEventListener('click', function (event)
     event.preventDefault();
 
     //anlam
-    let money = document.getElementById('totalMoney');
-    const pay = document.getElementById('payMoney');
-
-    const accountNumber = document.getElementById('accountNumber').value;
-    let pinNumber = document.getElementById('pinNumber');
-    pinNumber = parseInt(pinNumber.value);
-    if (accountNumber.length != 10) {
-        alert('Invalid Account Number');
-        return;
-    }
-
-    if (pinNumber != 2205) {
-        alert('Invalid Pin');
-    }
-
-    //convert
-    const payMoney = parseInt(pay.value);
-    let totalM = parseInt(money.innerText);
-    totalM -= payMoney;
-    money.innerText = totalM;
+    const payMoney = getInt('payMoney');
+    //validity
+    validity('accountNumber', 'pinNumber');
+    subIntT('totalMoney', payMoney);
 
 });
 
@@ -135,53 +87,86 @@ document.getElementById('payBillBtn').addEventListener('click', function (event)
 document.getElementById('Add-Button')
     .addEventListener('click', function (event) {
         event.preventDefault();
-
+        const buttons = document.getElementsByClassName('Button');
+        for (const button of buttons) {
+            button.style.display = 'none';
+        }
         document.getElementById('Add-Feature').style.display = "block";
-        document.getElementById('Cash-Feature').style.display = "none";
-        document.getElementById('Pay-Feature').style.display = "none";
-        document.getElementById('Transfer-Feature').style.display = "none";
-        document.getElementById('Transaction-Feature').style.display = "none";
-        document.getElementById('Bonus-Feature').style.display = "none";
+        const cardButtons = document.getElementsByClassName('card-button');
+        for (const btn of cardButtons) {
+            btn.classList.remove('border-[#0874f2]', 'bg-[#0874f20d]');
+            btn.classList.add('border-gray-300')
+        }
+        document.getElementById('Add-Button').classList.remove('border-gray-300');
+        document.getElementById('Add-Button').classList.add('border-[#0874f2]', 'bg-[#0874f20d]');
+        
+
     });
 document.getElementById('Cash-Button')
     .addEventListener('click', function (event) {
         event.preventDefault();
-        document.getElementById('Add-Feature').style.display = "none";
+        const buttons = document.getElementsByClassName('Button');
+        for (const button of buttons) {
+            button.style.display = 'none';
+        }
         document.getElementById('Cash-Feature').style.display = "block";
-        document.getElementById('Pay-Feature').style.display = "none";
-        document.getElementById('Transfer-Feature').style.display = "none";
-        document.getElementById('Transaction-Feature').style.display = "none";
-        document.getElementById('Bonus-Feature').style.display = "none";
+        const cardButtons = document.getElementsByClassName('card-button');
+        for (const btn of cardButtons) {
+            btn.classList.remove('border-[#0874f2]', 'bg-[#0874f20d]');
+            btn.classList.add('border-gray-300')
+        }
+        document.getElementById('Cash-Button').classList.remove('border-gray-300');
+        document.getElementById('Cash-Button').classList.add('border-[#0874f2]', 'bg-[#0874f20d]');
+        
     });
 document.getElementById('Transfer-Button')
     .addEventListener('click', function (event) {
         event.preventDefault();
-        document.getElementById('Add-Feature').style.display = "none";
-        document.getElementById('Cash-Feature').style.display = "none";
-        document.getElementById('Pay-Feature').style.display = "none";
+        const buttons = document.getElementsByClassName('Button');
+        for (const button of buttons) {
+            button.style.display = 'none';
+        }
         document.getElementById('Transfer-Feature').style.display = "block";
-        document.getElementById('Transaction-Feature').style.display = "none";
-        document.getElementById('Bonus-Feature').style.display = "none";
+        const cardButtons = document.getElementsByClassName('card-button');
+        for (const btn of cardButtons) {
+            btn.classList.remove('border-[#0874f2]', 'bg-[#0874f20d]');
+            btn.classList.add('border-gray-300')
+        }
+        document.getElementById('Transfer-Button').classList.remove('border-gray-300');
+        document.getElementById('Transfer-Button').classList.add('border-[#0874f2]', 'bg-[#0874f20d]');
+        
     });
 document.getElementById('Bonus-Button')
     .addEventListener('click', function (event) {
         event.preventDefault();
-        document.getElementById('Add-Feature').style.display = "none";
-        document.getElementById('Cash-Feature').style.display = "none";
-        document.getElementById('Pay-Feature').style.display = "none";
-        document.getElementById('Transfer-Feature').style.display = "none";
-        document.getElementById('Transaction-Feature').style.display = "none";
+        const buttons = document.getElementsByClassName('Button');
+        for (const button of buttons) {
+            button.style.display = 'none';
+        }
         document.getElementById('Bonus-Feature').style.display = "block";
+        const cardButtons = document.getElementsByClassName('card-button');
+        for (const btn of cardButtons) {
+            btn.classList.remove('border-[#0874f2]', 'bg-[#0874f20d]');
+            btn.classList.add('border-gray-300')
+        }
+         document.getElementById('Bonus-Button').classList.remove('border-gray-300');
+        document.getElementById('Bonus-Button').classList.add('border-[#0874f2]', 'bg-[#0874f20d]');
+       
     });
 document.getElementById('Pay-Button')
     .addEventListener('click', function (event) {
         event.preventDefault();
-        document.getElementById('Add-Feature').style.display = "none";
-        document.getElementById('Cash-Feature').style.display = "none";
+        const buttons = document.getElementsByClassName('Button');
+        for (const button of buttons) {
+            button.style.display = 'none';
+        }
         document.getElementById('Pay-Feature').style.display = "block";
-        document.getElementById('Transfer-Feature').style.display = "none";
-        document.getElementById('Transaction-Feature').style.display = "none";
-        document.getElementById('Bonus-Feature').style.display = "none";
+        const cardButtons = document.getElementsByClassName('card-button');
+        for (const btn of cardButtons) {
+            btn.classList.remove('border-[#0874f2]', 'bg-[#0874f20d]');
+            btn.classList.add('border-gray-300')
+        }
+        document.getElementById('Pay-Button').classList.remove('border-gray-300');
+        document.getElementById('Pay-Button').classList.add('border-[#0874f2]', 'bg-[#0874f20d]');
+        
     });
-
-
